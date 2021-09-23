a565438160753eb32f2d3da10f4090e7227d0935
Callie <ccwalker@loyola.edu>
2021-09-23 10:30:31 -0400

    Finished Lab 2 - Lesdy and Callie

--- .github/lab2.py
+++ .github/lab2.py
@@ -5,11 +5,11 @@
 # Program Inputs: Births, Deaths, Migrations, and total population
 # Program Outputs: Estimated population
 
-population = float(input("Enter the current population "))
-births = float(input("Enter the amount of births per second "))
-deaths = float(input("Enter the amount of deaths  per second "))
-migration = float(input("Enter the amount of migrations per second "))
-years = float(input("Enter the future amount of years "))
+population = float(input("Enter the current population: "))
+births = float(input("Enter the amount of births per second: "))
+deaths = float(input("Enter the amount of deaths  per second: "))
+migration = float(input("Enter the amount of migrations per second: "))
+years = float(input("Enter the future amount of years: "))
 
 daysInYear = 365 * years
 totalBirths = (((births*60) * 60) * 24) * daysInYear
@@ -19,7 +19,7 @@ estimatedPop =  estimatedPop = population + totalBirths - totalDeaths + totalMig
 if estimatedPop < 0:
     print("The population has died out")
 elif estimatedPop > 0:
-    print("Estimated population in ", years, " is ", estimatedPop)
+    print("Estimated population in ", years, " years is ", estimatedPop)
 
 
 
